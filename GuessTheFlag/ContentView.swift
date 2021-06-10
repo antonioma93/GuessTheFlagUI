0033346b5541bed966dd8988ac01384232902fb8
Antonio Massa <antonio.massa@zehus.it>
2021-06-10 08:02:01 +0200

    Re made the content view, now is working properly

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,22 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var countries = ["Estonia","France","Germany","Ireland","Italy","Nigeria","Monaco","Russia","Spain","UK","US","Poland"]
-        .shuffled()
-    @State private var correctAnswer = Int.random(in: 0...2)
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var userScore = 0
     
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.black]), startPoint: .top, endPoint: .bottom)
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .top, endPoint: .bottom)
                 .edgesIgnoringSafeArea(.all)
             VStack(spacing: 30) {
                 VStack {
                     Text("Tap the flag of")
                         .foregroundColor(.white)
+                        .font(.title)
+                    
                     Text(countries[correctAnswer])
                         .font(.largeTitle)
                         .fontWeight(.black)
@@ -31,22 +32,24 @@ struct ContentView: View {
                 }
                 ForEach(0 ..< 3) { number in
                     Button(action: {
-                        //Tapped flag
+                        //Flag tapped
                         self.flagTapped(number)
-            
                     }) {
                         Image(self.countries[number])
                             .renderingMode(.original)
                             .clipShape(Capsule())
                             .overlay(Capsule().stroke(Color.black, lineWidth: 1))
-                            .shadow(color: .black, radius: 5)
+                            .shadow(color: .black, radius: 2)
                     }
                 }
+                Text("Your score is: \(userScore)")
+                    .font(.headline)
                 Spacer()
             }
+            
         }
         .alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: Text("Your score is \(userScore + correctAnswer)"), dismissButton: .default(Text("Continue")) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is: \(userScore)"), dismissButton: .default(Text("Continue")) {
                 self.askQuestion()
             })
         }
@@ -54,9 +57,11 @@ struct ContentView: View {
     
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
-            scoreTitle = "Correct! Go ahed"
+            scoreTitle = "Correct"
+            userScore += 1
         } else {
-            scoreTitle = "Wrong! That's the \(countries[number]) flag!"
+            scoreTitle = "Wrong! This is \(countries[number])'s flag!"
+            userScore -= 1
         }
         showingScore = true
     }
