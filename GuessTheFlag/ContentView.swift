86dc691ebdc288dadfee76fd74b2c0e0be52ee8a
Antonio Massa <antonio.massa@zehus.it>
2021-06-11 16:53:00 +0200

    Added rotation effect for the flag

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,13 +7,30 @@
 
 import SwiftUI
 
+struct FlagImage: View {
+    var image: String
+    
+    var body: some View {
+        Image(image)
+            .renderingMode(.original)
+            .clipShape(Capsule())
+            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+            .shadow(color: .black, radius: 2)
+    }
+}
+
 struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var userScore = 0
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    
     @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var isCorrect = false
+    @State private var isWrong = false
+    @State private var selectedNumber = 0
+    @State private var fadeOutOpacity = false
     
     var body: some View {
         ZStack {
@@ -30,20 +47,29 @@ struct ContentView: View {
                         .fontWeight(.black)
                         .foregroundColor(.white)
                 }
+                
                 ForEach(0 ..< 3) { number in
                     Button(action: {
-                        //Flag tapped
-                        self.flagTapped(number)
+                        withAnimation {
+                            self.flagTapped(number)
+                        }
+                        
                     }) {
-                        Image(self.countries[number])
-                            .renderingMode(.original)
-                            .clipShape(Capsule())
-                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
-                            .shadow(color: .black, radius: 2)
+                        FlagImage(image: self.countries[number])
+                        
                     }
+                    .rotation3DEffect(.degrees(self.isCorrect && self.selectedNumber == number ? 360 : 0),
+                                      axis: (x: 0, y: 1, z: 0))
+                    .opacity(self.fadeOutOpacity && !(self.selectedNumber == number) ? 0.25 : 1)
+                    .rotation3DEffect(.degrees(self.isWrong && self.selectedNumber == number ? 90 : 0),
+                                      axis: (x: 0, y: 0, z: 0.5))
                 }
-                Text("Your score is: \(userScore)")
-                    .font(.headline)
+                HStack {
+                    Text("Your score is: \(userScore)")
+                        .foregroundColor(.black)
+                        .font(.title)
+                        .fontWeight(.black)
+                }
                 Spacer()
             }
             
@@ -56,12 +82,17 @@ struct ContentView: View {
     }
     
     func flagTapped(_ number: Int) {
+        self.selectedNumber = number
         if number == correctAnswer {
             scoreTitle = "Correct"
             userScore += 1
+            isCorrect = true
+            fadeOutOpacity = true
         } else {
             scoreTitle = "Wrong! This is \(countries[number])'s flag!"
             userScore -= 1
+            isWrong = true
+            fadeOutOpacity = true
         }
         showingScore = true
     }
@@ -69,6 +100,9 @@ struct ContentView: View {
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        isCorrect = false
+        fadeOutOpacity = false
+        isWrong = false
     }
 }
 
